cb02bad781638b0a635dcb3766c44396676f6f6b
nhfruchter <nhfruchter@cmu-840944.wv.cc.cmu.edu>
2014-07-14 16:45:07 -0400

    Beginnings of user defined keyword matching

---
+++ bro/config/custom.py
@@ -0,0 +1,10 @@
+"""Custom settings for sessions."""
+from keywords import *
+          
+user_keywords = [
+    Keyword("Carnegie Mellon"),
+    Keyword("cmu"),
+    Keyword("Nathaniel Fruchter"),
+    Keyword("Fruchter"),
+    Phone(1, 512, 961, 6043)   
+]
